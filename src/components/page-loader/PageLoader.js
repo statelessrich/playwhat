b7ecb69f351400c8243fb5727507fb0ece783338
Richard Pires <richardjpires@gmail.com>
2022-09-20 18:21:34 +0200

    Added spinner to loading page. Added open sans fallback font to app.

--- src/components/page-loader/PageLoader.js
+++ src/components/page-loader/PageLoader.js
@@ -4,7 +4,14 @@ import styles from "./page-loader.module.scss";
 const PageLoader = () => {
   return (
     <div className={styles.pageLoader}>
-      <h1>playwhat</h1> 
+      <h1>playwhat</h1>
+
+      <div className={styles.ldsEllipsis}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
